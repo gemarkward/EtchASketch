0b90f4bace0ace88f23334ad81b221586ea6b915
Gilbert Markward <gilbert.markward@ymail.com>
2023-03-23 14:52:00 -0400

    Fix mouseover issue
    I had to add the mouseover function after the game board was generated.

--- eas.js
+++ eas.js
@@ -1,25 +1,36 @@
-function generateBoard(n = 16)
+$(function()
 {
-    // Initialize gameBoard, assign game's <div>
-    var gameBoard = document.getElementById("gameBoard");
-    // Iterate for n rows
-    for (i=0;i<n;i++)
+    function generateBoard(n = 16)
     {
-        // Create row object
-        var row = document.createElement("div");
-        row.className = "row";
-        // Iterate for n columns
-        for (j=0;j<n;j++)
+        if (n > 100)
         {
-            // Create gameButton object
-            var gameButton = document.createElement("div");
-            gameButton.className = "button";
-            // Append gameButton to row
-            row.appendChild(gameButton);
+            n = 100;
         };
-        // Append row to gameBoard
-        gameBoard.appendChild(row);
+        // Initialize gameBoard, assign game's <div>
+        var gameBoard = document.getElementById("gameBoard");
+        // Iterate for n rows
+        for (i=0;i<n;i++)
+        {
+            // Create row object
+            var row = document.createElement("div");
+            row.className = "row";
+            // Iterate for n columns
+            for (j=0;j<n;j++)
+            {
+                // Create gameButton object
+                var gameButton = document.createElement("div");
+                gameButton.className = "button";
+                row.appendChild(gameButton);
+            };
+            // Append row to gameBoard
+            gameBoard.appendChild(row);
+        };
     };
-    // Populate objects to html
+    generateBoard();
+    $(".button")
+    .mouseover(function(){
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        $(this).css("background-color","#"+randomColor);
+    });
+});
 
-};
