49f40d6afe42a79d1dc833d79de9cc0ab91a82f1
Gilbert Markward <gilbert.markward@ymail.com>
2023-03-24 10:25:59 -0400

    Add cancel option to generateBoard Autogenerate on reload

--- eas.js
+++ eas.js
@@ -1,42 +1,69 @@
 $(function()
 {
-    function generateBoard()
+    const isNumber = function isNumber(value) 
     {
-        // Prompt user for number of sides
-        var n = prompt("How many pixels per side would you like?")
-        // Make sure n is a maximum of 100
-        if (n > 100)
+   return typeof value === 'number' && isFinite(value);
+    }
+    function generateBoard(n)
+    {
+        // Allow for autogenerate on reload without input prompt
+        if (n == null)
         {
-            n = 100;
+            // Prompt user for number of sides
+            var n = prompt("How many pixels per side would you like?");
         };
-        // Initialize gameBoard, assign game's <div>
-        var gameBoard = document.getElementById("gameBoard");
-        // Clear the gameboard
-        gameBoard.innerHTML = "";
-        // Iterate for n rows
-        for (i=0;i<n;i++)
+        // Check input for cancel button
+        if (n != null)
         {
-            // Create row object
-            var row = document.createElement("div");
-            row.className = "row";
-            // Iterate for n columns
-            for (j=0;j<n;j++)
+            // Check for blank input and set default to 16
+            if (n === "")
             {
-                // Create gameButton object
-                var gameButton = document.createElement("div");
-                gameButton.className = "button";
-                row.appendChild(gameButton);
+                n = 16;
+            }
+            // Make sure input is a number
+            else if (!isNumber(n))
+            {
+                alert("Input must be a number between 1 and 100!");   
+            }
+            // Check for value over 100, if so set to 100
+            else if (n > 100)
+            {
+                n = 100;
             };
-            // Append row to gameBoard
-            gameBoard.appendChild(row);
+            // Initialize gameBoard, assign game's <div>
+            var gameBoard = document.getElementById("gameBoard");
+            // Clear the gameboard
+            gameBoard.innerHTML = "";
+            // Iterate for n rows
+            for (i=0;i<n;i++)
+            {
+                // Create row object
+                var row = document.createElement("div");
+                row.className = "row";
+                // Iterate for n columns
+                for (j=0;j<n;j++)
+                {
+                    // Create gameButton object
+                    var gameButton = document.createElement("div");
+                    gameButton.style.backgroundColor = "white";
+                    gameButton.className = "button";
+                    row.appendChild(gameButton);
+                };
+                // Append row to gameBoard
+                gameBoard.appendChild(row);
+            };
         };
+
+        // Set button class object to random bgcolor on mouseover
         $(".button")
         .mouseover(function(){
             var randomColor = Math.floor(Math.random()*16777215).toString(16);
             $(this).css("background-color","#"+randomColor);
         });
     };
-    generateBoard();
+
+    // Generate new board on reload
+    generateBoard(16);
     document.getElementById("generate").onclick = function() {generateBoard()};
 });
 
