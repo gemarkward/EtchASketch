4b2008ccf05b9a19b29398e14e869e3c4b044e07
Gilbert Markward <gilbert.markward@ymail.com>
2023-03-24 08:23:51 -0400

    Add generate button and fit to screen

--- eas.js
+++ eas.js
@@ -1,13 +1,18 @@
 $(function()
 {
-    function generateBoard(n = 16)
+    function generateBoard()
     {
+        // Prompt user for number of sides
+        var n = prompt("How many pixels per side would you like?")
+        // Make sure n is a maximum of 100
         if (n > 100)
         {
             n = 100;
         };
         // Initialize gameBoard, assign game's <div>
         var gameBoard = document.getElementById("gameBoard");
+        // Clear the gameboard
+        gameBoard.innerHTML = "";
         // Iterate for n rows
         for (i=0;i<n;i++)
         {
@@ -25,12 +30,13 @@ $(function()
             // Append row to gameBoard
             gameBoard.appendChild(row);
         };
+        $(".button")
+        .mouseover(function(){
+            var randomColor = Math.floor(Math.random()*16777215).toString(16);
+            $(this).css("background-color","#"+randomColor);
+        });
     };
-    generateBoard(100);
-    $(".button")
-    .mouseover(function(){
-        var randomColor = Math.floor(Math.random()*16777215).toString(16);
-        $(this).css("background-color","#"+randomColor);
-    });
+    generateBoard();
+    document.getElementById("generate").onclick = function() {generateBoard()};
 });
 
